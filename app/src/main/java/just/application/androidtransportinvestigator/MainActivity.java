b203ce042cbdb039e4c987bad8c59abe14faa7d8
KVGrygoriev <KVGrygoriev@luxoft.com>
2018-10-05 13:25:57 +0300

    Defined listeners for transport's switches

--- app/src/main/java/just/application/androidtransportinvestigator/MainActivity.java
+++ app/src/main/java/just/application/androidtransportinvestigator/MainActivity.java
@@ -1,13 +1,55 @@
 package just.application.androidtransportinvestigator;
 
+import just.application.androidtransportinvestigator.Logger;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Switch swtchBtSupport, swtchUsbSupport, swtchTcpSupport;
+    TextView logger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swtchBtSupport = (Switch) findViewById(R.id.swtchBtSupport);
+        swtchUsbSupport = (Switch) findViewById(R.id.swtchUsbSupport);
+        swtchTcpSupport = (Switch) findViewById(R.id.swtchTcpSupport);
+
+        logger = (TextView) findViewById(R.id.loggerDisplay);
+
+        registerListener();
+    }
+
+    private void registerListener() {
+        swtchBtSupport.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Logger.Debug(logger, TAG, buttonView.getText() + " got state " + (isChecked ? "CHECKED" : "UNCHECKED"));
+            }
+        });
+
+        swtchUsbSupport.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Logger.Debug(logger, TAG, buttonView.getText() + " got state " + (isChecked ? "CHECKED" : "UNCHECKED"));
+            }
+        });
+
+        swtchTcpSupport.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Logger.Debug(logger, TAG, buttonView.getText() + " got state " + (isChecked ? "CHECKED" : "UNCHECKED"));
+            }
+        });
     }
 }
