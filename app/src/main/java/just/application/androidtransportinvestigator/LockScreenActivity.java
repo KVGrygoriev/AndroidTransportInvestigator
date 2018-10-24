e2a5aef4c9e4e402f6d2480427c3e16517a35d1a
KVGrygoriev <KVGrygoriev@luxoft.com>
2018-10-24 14:19:44 +0300

    Provided SdlApplication

--- app/src/main/java/just/application/androidtransportinvestigator/LockScreenActivity.java
+++ app/src/main/java/just/application/androidtransportinvestigator/LockScreenActivity.java
@@ -1,9 +1,14 @@
 package just.application.androidtransportinvestigator;
 
+import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LockScreenActivity extends AppCompatActivity {
+
+    public static void registerActivityLifecycle(Application application) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
