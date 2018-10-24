1a1ba47644bbc354c81d4c235d94ce968cd29981
KVGrygoriev <KVGrygoriev@luxoft.com>
2018-10-24 14:09:49 +0300

    LockScreenActivity barebone

---
+++ app/src/main/java/just/application/androidtransportinvestigator/LockScreenActivity.java
@@ -0,0 +1,19 @@
+package just.application.androidtransportinvestigator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LockScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
